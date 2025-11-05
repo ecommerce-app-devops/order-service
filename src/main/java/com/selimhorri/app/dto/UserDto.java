5c5566e6585b60046ddb5288147a86b3e58a229c
AIejoCastro <alejocastro20@hotmail.com>
2025-11-04 20:41:55 -0500

    Fix service

--- src/main/java/com/selimhorri/app/dto/UserDto.java
+++ src/main/java/com/selimhorri/app/dto/UserDto.java
@@ -39,4 +39,3 @@ public class UserDto implements Serializable {
 
 
 
-
--- src/main/java/com/selimhorri/app/resource/CartResource.java
+++ src/main/java/com/selimhorri/app/resource/CartResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,27 +52,6 @@ public class CartResource {
 		return ResponseEntity.ok(this.cartService.save(cartDto));
 	}
 	
-	@PutMapping
-	public ResponseEntity<CartDto> update(
-			@RequestBody 
-			@NotNull(message = "Input must not be NULL") 
-			@Valid final CartDto cartDto) {
-		log.info("*** CartDto, resource; update cart *");
-		return ResponseEntity.ok(this.cartService.update(cartDto));
-	}
-	
-	@PutMapping("/{cartId}")
-	public ResponseEntity<CartDto> update(
-			@PathVariable("cartId")
-			@NotBlank(message = "Input must not be blank")
-			@Valid final String cartId,
-			@RequestBody 
-			@NotNull(message = "Input must not be NULL") 
-			@Valid final CartDto cartDto) {
-		log.info("*** CartDto, resource; update cart with cartId *");
-		return ResponseEntity.ok(this.cartService.update(Integer.parseInt(cartId), cartDto));
-	}
-	
 	@DeleteMapping("/{cartId}")
 	public ResponseEntity<Boolean> deleteById(@PathVariable("cartId") final String cartId) {
 		log.info("*** Boolean, resource; delete cart by id *");
@@ -84,7 +62,6 @@ public class CartResource {
 	
 	
 }
-
 
 
 
